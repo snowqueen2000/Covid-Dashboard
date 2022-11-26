9e3c0e2883ead8b666982ea8c58db972e18ea05c
Snow Queen <71298338+snowqueen2000@users.noreply.github.com>
2022-11-26 14:42:44 -0700

    scraping

--- ScrapingV1.py
+++ ScrapingV1.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-link = 'https://www.worldometers.info/coronavirus/'
+link = 'https://www.worldometers.info/coronavirus/#countries'
 data = requests.get(link)
 soup = BeautifulSoup(data.text, 'lxml')
-print(soup)
 
+# finding the main three stats on the dashboard
+mainStats = soup.find_all('div', {"id": "maincounter-wrap"})
+print(mainStats)
 
 
 
