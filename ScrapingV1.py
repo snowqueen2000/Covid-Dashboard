47832a4b9facdcb150e7acce6c1fddf59a98d25c
Snow Queen <71298338+snowqueen2000@users.noreply.github.com>
2022-11-26 15:07:18 -0700

    dashboard woohoo

--- ScrapingV1.py
+++ ScrapingV1.py
@@ -7,22 +7,15 @@ data = requests.get(link)
 soup = BeautifulSoup(data.text, 'lxml')
 
 # finding the main three stats on the dashboard
-mainStats = soup.find_all('div', {"id": "maincounter-wrap"})
+mainStats = soup.find_all('div', {"id": "maincounter-wrap"}) # stats from the main  covid dashboard
 
-mainStatList = []
-statNameList = []
-for stat in mainStats:
-    mainStat = stat.span.text
-    statName = stat.h1.text
-    mainStatList.append(mainStat)
+mainStatList = [] # list of the stat NUMBERS (must go outside the for loop)
+statNameList = [] # list of the corresponding stat NAMES
+for stat in mainStats: 
+    mainStat = stat.span.text # get text of the span header
+    statName = stat.h1.text # get text of the h1 header
+    mainStatList.append(mainStat) 
     statNameList.append(statName)
-
-"""""
-statNameList = []
-for name in statNames:
-    statName = name.h1.text
-    statNameList.append(statName)
-"""""
     
 print(mainStatList)
 print(statNameList)
