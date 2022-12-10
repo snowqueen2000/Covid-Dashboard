85cb5c24dc18c4837248391afdef4272d456534a
Snow Queen <71298338+snowqueen2000@users.noreply.github.com>
2022-12-09 19:32:10 -0700

    moving around files and renaming

--- Plot.py
+++ Plot.py
@@ -3,7 +3,7 @@ from bokeh.models import ColumnDataSource, TableColumn, DataTable
 from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
 from bokeh.io import show
 import pandas as pd
-from MakeDash import cD
+from Dash import cD
 
 # read in country csv
 # SouthKorea = pd.read_csv('South Korea.txt')
