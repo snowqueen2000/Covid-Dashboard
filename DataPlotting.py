7b5be956f95f444866746695a699b2f0fe81f610
Snow Queen <71298338+snowqueen2000@users.noreply.github.com>
2022-12-10 15:59:49 -0700

    data frames are goated

--- DataPlotting.py
+++ DataPlotting.py
@@ -15,16 +15,60 @@ show(p)
 """
 
 import json
-  
+import pandas as pd
+
 # Opening JSON file
 f = open('covid_data.json')
   
 # returns JSON object as a dictionary
 data = json.load(f)
-  
-# Iterating through the json list
-for i in data['Country,Other']:
-    print(i)
+data = pd.DataFrame(data)
+#print(data) 
   
 # Closing file
 f.close()
+
+'''
+p = figure(
+    title = 'Beep Boop Video Tutorial',
+    x_axis_label = 'X-Axis', 
+    y_axis_label = 'Y-Axis',  
+)
+
+# render glyph
+p.line(x, y, legend = 'Test!', line_width = 3)
+
+# graphing! 
+data.plot_bokeh(
+    kind = 'bar',
+    x = 'Country,Other',
+    y = ['TotalDeaths', 'Deaths/1M pop'],
+    xlabel = 'Country',
+    ylabel = 'Deaths',
+    title = 'Deaths per Country'
+)
+
+# show the plot! 
+show(p)
+'''
+
+from bokeh.plotting import figure, output_file, show
+  
+# file to save the model
+output_file("gfg.html")
+      
+# instantiating the figure object
+graph = figure(title = "Bokeh Vertical Bar Graph")
+  
+# x-coordinates of the top edges
+top = list(data['TotalDeaths'])
+top = [int(x) for x in top]
+print(top)
+
+# plotting the graph
+graph.vbar(x = list(data['Country,Other']),
+           top = top,
+           width = 0.5)
+  
+# displaying the model
+show(graph)
