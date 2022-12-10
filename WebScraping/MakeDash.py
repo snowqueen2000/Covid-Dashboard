7dcceb5e2711b29e9b8fb1a32decd4c0bba2797d
Snow Queen <71298338+snowqueen2000@users.noreply.github.com>
2022-12-09 19:29:56 -0700

    deleted Plotting

--- WebScraping/MakeDash.py
+++ WebScraping/MakeDash.py
@@ -7,6 +7,8 @@ country = "Brazil"
 firstinq = ScrapeWebsite()
 
 # print the results of the inquiry
-print(firstinq.scrape_country(url,country))
+#print(firstinq.scrape_country(url,country))
+
+cD = firstinq.scrape_country(url,country)
 # shouldn't running this create a text file in our directory that has the info for that country?? 
 # we have a south korea file somehow?? 
